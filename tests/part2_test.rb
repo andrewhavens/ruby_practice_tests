f9780c0287f7160405cb2a25d494737b91229dcb
Andrew Havens <andrew.havens@evanta.com>
2012-10-03 15:51:31 -0700

    added tests

--- tests/part2_test.rb
+++ tests/part2_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require '../part2'
 
-class rps_game_winner_tests < MiniTest::Unit::TestCase
+class RpsGameWinnerTests < MiniTest::Unit::TestCase
 
 	def test_requires_two_players
 		game = [['one', 'r'],['two','p'],['three','s']]
@@ -18,7 +18,17 @@ class rps_game_winner_tests < MiniTest::Unit::TestCase
 	end
 
 	def test_paper_beats_rock
-		game = [['one', 'p'],['two','r']]
+		game = [['one', 'p'],['two','r']].shuffle
 		assert_equal ['one', 'p'], rps_game_winner(game)
+	end
+
+	def test_rock_beats_scissors
+		game = [['one', 'r'],['two','s']].shuffle
+		assert_equal ['one', 'r'], rps_game_winner(game)
+	end
+
+	def test_scissors_beats_paper
+		game = [['one', 's'],['two','p']].shuffle
+		assert_equal ['one', 's'], rps_game_winner(game)
 	end
 end
